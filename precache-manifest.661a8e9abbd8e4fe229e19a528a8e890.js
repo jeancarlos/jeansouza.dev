84444b1bea87a05ce88a12194a01b1c3cee7c7a2
Jean Souza <jean.co.souza@gmail.com>
2020-06-10 14:48:04 -0300

    Updates

---
+++ precache-manifest.661a8e9abbd8e4fe229e19a528a8e890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9b7737bd8a9553f3fdf1ec0eba6509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11e50eaf694adf7afa6b",
+    "url": "/static/css/main.ec1c65ce.chunk.css"
+  },
+  {
+    "revision": "8d3cd37592258eaea440",
+    "url": "/static/js/2.0f6d47ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f6d47ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e50eaf694adf7afa6b",
+    "url": "/static/js/main.dc504146.chunk.js"
+  },
+  {
+    "revision": "84ac65994d53f35e12bc",
+    "url": "/static/js/runtime-main.82de50f5.js"
+  }
+]);
