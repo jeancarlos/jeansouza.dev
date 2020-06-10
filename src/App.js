698b32d92e6523df6ee435a54d9c9976d5adcc13
Jean Souza <jean.co.souza@gmail.com>
2020-06-10 14:12:36 -0300

    temp version

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App--Header">
+        <h1 className="App--Title">
+          Hi, I'm <strong>Jean</strong>, <span class="App--TitleMyRole">Front-end Engineer</span>
+          <span class="App--TitlePonctuation">.</span>
+        </h1>
+        <p className="App-Links">
+          <a href="https://github.com/jeancarlos">GitHub</a>
+          <a href="https://www.linkedin.com/in/jeancarlosudi/">LinkedIn</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
