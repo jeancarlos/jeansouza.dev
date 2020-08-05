920ead932831377d7f1e145216853b4fcebaeb2f
Jean Souza <jean.co.souza@gmail.com>
2020-08-05 14:42:03 -0300

    update emoji and loading

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import emoji from 'constants/appleEmoji'
 
-function Emoji({ label, symbol }) {
+function Emoji({ label, symbol, className = '' }) {
+  let parseSymbol = symbol
+  const spanEl = useRef(null)
+  const [fontSize, setFontSize] = useState('1rem')
+
+  useEffect(() => {
+    if (emoji[symbol] && spanEl.current) {
+      setFontSize(window.getComputedStyle(spanEl.current)['font-size'])
+    }
+  }, [symbol]);
+
+  if (emoji[symbol]) {
+    parseSymbol = (<img style={{ width: fontSize }} alt="" src={emoji[symbol]} />)
+  }
+
   return (
     <span
-      className="emoji"
+      ref={spanEl}
+      className={`${className} emoji`}
       role="img"
       aria-label={label || ''}
       aria-hidden={(!!label).toString()}
     >
-      {symbol}
+      {parseSymbol}
     </span>
   )
 }
