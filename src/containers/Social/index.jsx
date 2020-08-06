553c239dc3ff7d7ad11490f3ce9d45e9294c4786
Jean Souza <jean.co.souza@gmail.com>
2020-08-06 03:25:23 -0300

    update social and tootip compenet

--- src/containers/Social/index.jsx
+++ src/containers/Social/index.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
+import Tooltip from 'components/Tooltip/index'
 import Emoji from 'components/Emoji'
 import './style.css';
 
@@ -22,21 +23,65 @@ const links = [
   {
     name: 'LinkedIn',
     url: 'https://www.linkedin.com/in/jeancarlosudi/'
+  },
+  {
+    name: 'steam',
+    url: 'https://steamcommunity.com/r/leonrk'
+  },
+  {
+    name: 'Switch Friend Code',
+    content: '1749 2384 1794'
+  },
+  {
+    name: 'The source code of this site',
+    url: 'https://github.com/jeancarlos/mysite'
   }
 ]
 
 function Social() {
+  const [showTooltip, setShowTooltip] = useState([])
+
+  const handleClick = (event, index, force) => {
+    const isForceBoolean = typeof force === 'boolean'
+    const forceValue = (value) => isForceBoolean ? force : value
+    const newShowValue = (value, i) => index === i ? forceValue(!value) : value
+
+    setShowTooltip(values => values.map(newShowValue))
+    event.preventDefault()
+  }
+
+  useEffect(() => {
+    setShowTooltip(links.map(() => false))
+  }, [])
+
+
+  const link = ({ name, url, content }, index) => (
+    <li key={name} className="Social__ListItem">
+      <span className="Social__Bullet">*</span>
+      {
+        content ? [
+          <a key="a" onClick={(e) => handleClick(e, index)} href="#pop"> {name}</a>,
+          <Tooltip
+            key="Tooltip"
+            onMouseLeave={(e) => handleClick(e, index, false)}
+            component='span' show={showTooltip[index]}
+          >
+            {content}
+          </Tooltip>
+        ] : <a rel="noopener noreferrer" href={url}> {name}</a>
+      }
+    </li >
+  )
+
   return (
-    <Fragment>
-      <header className="App--Header">
-        <h1 className="App--Title">
-          <Emoji symbol="🔗" /> Social & Links
+    <header className="App--Header">
+      <h1 className="App--Title">
+        <Emoji symbol="🔗" /> Social & Links
         </h1>
-        <ul className="App-Links Social__List">
-          {links.map(({ name, url }) => <li key={name}><span class="Social__Bullet">*</span><a rel="noopener noreferrer" href={url}> {name}</a></li>)}
-        </ul>
-      </header>
-    </Fragment>
+      <ul className="App-Links Social__List">
+        {links.map(link)}
+      </ul>
+    </header>
   );
 }
 
