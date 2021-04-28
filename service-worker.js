99e024de6729e01a8c21abf1963347bb1a2a8500
jeancarlos <jean.co.souza@gmail.com>
2021-04-28 10:35:55 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.925a62a3b032180724e369c3b7bdab38.js"
+  "/precache-manifest.85b25c1319f5b2d974041e1ab40e7b0e.js"
 );
 
 self.addEventListener('message', (event) => {
