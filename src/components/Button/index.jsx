7645b9c5f18ab40dfcc8299351b23cdc653b8f38
jeancarlos <jean.co.souza@gmail.com>
2021-08-14 13:55:38 -0300

    feat: new design

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,18 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import './style.styl'
+
+const hoverClass = (hover) => {
+  if (typeof hover === 'undefined') {
+    return ''
+  }
+
+  return hover ? 'hoverIn' : 'hoverOut'
+}
 
 function Button ({ children, href, onClick, className = '' }) {
   const url = href || '#'
+  const [hoverState, setHoverState] = useState()
   const rel = href ? 'noopener noreferrer' : ''
 
   const handleClick = (e) => {
@@ -15,10 +24,14 @@ function Button ({ children, href, onClick, className = '' }) {
 
   return (
     <a
-      className={`${className} Button`}
+      className={`${className} ${hoverClass(hoverState)} Button`}
       rel={rel}
       href={url}
       onClick={handleClick}
+      onFocus={() => setHoverState(true)}
+      onBlur={() => setHoverState(false)}
+      onMouseEnter={() => setHoverState(true)}
+      onMouseLeave={() => setHoverState(false)}
     >
       {children}
     </a>
---
+++ src/containers/Articles/como-fiz-meu-blog.md
@@ -0,0 +1,8 @@
+---
+title: Fiz esse blog usando MDX, Next.js e React
+language: pt-br
+description: teste
+publishedOn: 2021-08-06T17:17:49.607Z
+---
+
+TEste
