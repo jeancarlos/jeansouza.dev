4dfcdefbc529da91845293cf932e68df84832e46
jeancarlos <jean.co.souza@gmail.com>
2026-04-07 02:33:28 -0300

    chore: remove remaining legacy dead code — jsx duplicates, styl files, CRA config

--- src/constants/appleEmoji.js
+++
@@ -1,6 +0,0 @@
-const appleEmoji = {
-  '🔗': 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/apple/237/link-symbol_1f517.png',
-  '⌛': 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/237/hourglass_231b.png'
-}
-
-export default appleEmoji
